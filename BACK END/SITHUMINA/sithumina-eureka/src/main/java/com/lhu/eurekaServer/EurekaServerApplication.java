743a95a197714d1962c3ce11d9811d6b7cc2b042
Lahiru <=>
2021-06-25 00:14:47 +0530

    eureka commit

---
+++ BACK END/SITHUMINA/sithumina-eureka/src/main/java/com/lhu/eurekaServer/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.lhu.eurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
